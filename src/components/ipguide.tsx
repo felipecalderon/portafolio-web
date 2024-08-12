5b6b80dc78b58d0ea91ac06708e7907f00fd7a86
Felipe <felipe.calderon321@gmail.com>
2024-08-12 10:43:51 -0400

    weather api

--- src/components/ipguide.tsx
+++ src/components/ipguide.tsx
@@ -23,25 +23,60 @@ interface ResponseIP {
         longitude: number
     } | null
 }
+
+interface ResponseWeather {
+    coord: { lon: number; lat: number }
+    weather: [
+        {
+            id: number
+            main: string
+            description: string
+            icon: string
+        }
+    ]
+    base: string
+    main: {
+        temp: number
+        feels_like: number
+        temp_min: number
+        temp_max: number
+        pressure: number
+        humidity: number
+        sea_level: number
+        grnd_level: number
+    }
+    visibility: number
+    wind: { speed: number; deg: number; gust: number }
+    clouds: { all: number }
+    dt: number
+    sys: { country: string; sunrise: number; sunset: number }
+    timezone: number
+    id: number
+    name: string
+    cod: number
+}
+
 export default async function IPData() {
     const header = headers()
     const ip = (header.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0]
     console.log(header.get("x-forwarded-for"))
 
-    if (ip.includes(":")) return null
-
     const res = await fetch(`https://ip.guide/${ip}`)
     const data: ResponseIP = await res.json()
+    if (!data.location) return null
 
-    if (data.location) {
+    const resWeather = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${data.location.latitude}&lon=${data.location.longitude}&appid=${process.env.CLIMA_API}`
+    )
+    const weather: ResponseWeather = await resWeather.json()
+
+    if (data.location && weather) {
         return (
-            <div>
-                <p>{data.location.city}</p>
-                <p>{data.location.country}</p>
-                <p>{data.location.timezone}</p>
-                <p>{data.location.latitude}</p>
-                <p>{data.location.longitude}</p>
-                <p>{data.network.autonomous_system.name}</p>
+            <div className='text-center'>
+                <p>Ciudad: {data.location.city}</p>
+                <p>Temperatura actual: {(weather.main.temp - 273.15).toFixed(2)}</p>
+                <p>Humedad: {weather.main.humidity}%</p>
+                <pre className='text-wrap'>{JSON.stringify(weather.weather[0])}</pre>
             </div>
         )
     }
