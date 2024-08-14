d1e3fab924fe0234f3ffb35bb8d046b78b731f64
Felipe <felipe.calderon321@gmail.com>
2024-08-14 11:59:54 -0400

    adding header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header className='bg-sky-900 text-white p-8 pb-32 text-center'>
             <h1 className='text-3xl font-bold'>Felipe Eduardo Calderón</h1>
             <p className='mt-2 text-xl'>Desarrollador Web FullStack</p>
-            <p className='mt-1'>Landing page | Ecommerce | Blogs | Sistemas | Apps</p>
+            <p className='mt-1 text-xs uppercase'>Landing page | Ecommerce | Blogs | Sistemas | Apps</p>
         </header>
     )
 }
