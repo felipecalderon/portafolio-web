bfb558501ef684c30b561e57932c56786252e49c
Felipe <felipe.calderon321@gmail.com>
2024-08-15 11:24:50 -0400

    refactor profile img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,11 @@ import Habilidades from "@/components/habilidades"
 import Header from "@/components/header"
 import Clima from "@/components/ipguide"
 import SobreMi from "@/components/sobre-mi"
-import { Image } from "@nextui-org/react"
 
 export default function Home() {
     return (
         <>
             <Header />
-            <div className='absolute inset-0 flex justify-center'>
-                <Image
-                    radius='full'
-                    className='w-52 h-52 object-cover transform left-1/2 -translate-x-[104px] translate-y-1 top-36 drop-shadow-2xl border-t-8 border-t-yellow-400 border-b-8 border-b-sky-600'
-                    src='https://res.cloudinary.com/duwncbe8p/image/upload/f_auto,q_auto/v1/portfolio/fa45nfrjf2hyhdzpbryu'
-                />
-            </div>
             <Clima />
 
             <main className='container mx-auto space-y-10 mt-10 p-8 bg-white rounded-lg max-w-2xl shadow-2xl'>
