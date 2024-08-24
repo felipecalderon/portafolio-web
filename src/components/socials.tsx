1a07a91256813b1a6cc4a9ae07bacca44427d1b4
Felipe <felipe.calderon321@gmail.com>
2024-08-24 14:09:27 -0400

    refactor gpt code

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -4,14 +4,14 @@ import { MdMarkEmailRead } from "react-icons/md"
 
 export default function SocialPages() {
     return (
-        <div className='flex gap-2 justify-center text-slate-50 dark:text-sky-800'>
+        <div className='flex gap-1 justify-center text-slate-50 dark:text-sky-800 text-sm md:text-base'>
             <Link
                 href='https://github.com/felipecalderon'
                 target='_blank'
                 rel='noopener noreferrer'
                 className='inline-flex gap-1 items-center py-1 px-2 bg-sky-700 dark:bg-slate-100 rounded-lg'
             >
-                <FaGithub className='text-xl' /> GitHub
+                <FaGithub /> GitHub
             </Link>
             <Link
                 href='https://www.facebook.com/felipecalderon321/'
@@ -19,7 +19,7 @@ export default function SocialPages() {
                 rel='noopener noreferrer'
                 className='inline-flex gap-1 items-center py-1 px-2 bg-sky-700 dark:bg-slate-100 rounded-lg'
             >
-                <FaFacebookF className='text-xl' /> Facebook
+                <FaFacebookF /> Facebook
             </Link>
             <Link
                 href='https://www.linkedin.com/in/felipecalderone/'
@@ -27,7 +27,7 @@ export default function SocialPages() {
                 rel='noopener noreferrer'
                 className='inline-flex gap-1 items-center py-1 px-2 bg-sky-700 dark:bg-slate-100 rounded-lg'
             >
-                <FaLinkedin className='text-xl' /> LinkedIn
+                <FaLinkedin /> LinkedIn
             </Link>
             <a
                 href='mailto:felipe.calderon321@gmail.com?Subject=Solicito un presupuesto de desarrollo digital'
@@ -35,7 +35,7 @@ export default function SocialPages() {
                 rel='noopener noreferrer'
                 className='inline-flex gap-1 items-center py-1 px-2 bg-sky-700 dark:bg-slate-100 rounded-lg'
             >
-                <MdMarkEmailRead className='text-xl' /> Correo
+                <MdMarkEmailRead /> Correo
             </a>
         </div>
     )
